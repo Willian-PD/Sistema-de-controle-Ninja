49af17f86c497961608d48e89048c9856bdb3402
Willian-PD <domingues221@gmail.com>
2020-11-26 19:33:44 -0300

    Correções

--- Sistema de RH de Konoha/src/App.java
+++
@@ -1,8 +0,0 @@
-//import Packages.bo.*;
-//import Packages.connect.*;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
